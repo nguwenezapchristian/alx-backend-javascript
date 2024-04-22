9b882cdccddcbb9ad03d4a35f2e950dd7a4a73f6
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-22 20:49:19 +0200

    added comment

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,4 @@
+// a function named createEmployeesObject that will receive two arguments
 export default function createEmployeesObject(departmentName, employees) {
   return { [departmentName]: employees, };
 }
