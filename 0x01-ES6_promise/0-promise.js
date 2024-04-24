df846ebf8f15465ed1632a8fc52145ec9aaa3050
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-24 11:06:00 +0200

    corrected indentation

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-
 export function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const i = 2;
-        if (i == 2) {
-            resolve('success');
-        } else {
-            reject('failed');
-        }
-    });
+  return new Promise();
 }
