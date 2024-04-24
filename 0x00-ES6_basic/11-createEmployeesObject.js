d391109b9dd8bd752c03d9fa7a272edd9054c332
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-24 13:13:09 +0200

    fixed eslint errors

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 // a function named createEmployeesObject that will receive two arguments
 export default function createEmployeesObject(departmentName, employees) {
-  return { 
+  return {
     [departmentName]: employees,
   };
 }
