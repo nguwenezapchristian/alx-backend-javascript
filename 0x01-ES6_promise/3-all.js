1f718200db0a43500e73a4d16cd649de3433829c
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-24 15:54:29 +0200

    used 2 indentantion

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-    return Promise
+  return Promise
     .all([uploadPhoto(), createUser()])
     .then((response) => {
-        console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
     })
-    .catch(() => {
-        console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
