dbe6d4fcad4c47073c1cae9edc591c1a7843c816
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-24 22:08:37 +0200

    removed unused var

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise
-    .all([uploadPhoto(), createUser()])
+    .all([uploadPhoto, createUser])
     .then((response) => {
       console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
     })
