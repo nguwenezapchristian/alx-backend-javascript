19b5508d2d195bc06ce9e6c0c05f79bd374976f7
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-24 13:39:42 +0200

    ejects the Promise with an Error object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -6,9 +6,7 @@ export default function getFullResponseFromAPI(success) {
         body: 'success'
       });
     } else {
-      reject({
-        Error: 'The fake API is not working currently'
-      });
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
