916d5567b2cd46db4da3eb0e699ecc579f3e6c0e
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-25 18:01:51 +0200

    removed setter method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -20,13 +20,6 @@ export default class Building {
         return this._sqft;
     }
 
-    set sqft(sqft) {
-        if (typeof sqft !== 'number') {
-            throw new TypeError('Sqft must be a number');
-        }
-        this._sqft = sqft; 
-    }
-
     evacuationWarningMessage() {
         throw new Error('Class extending Building must override evacuationWarningMessage');
     }
