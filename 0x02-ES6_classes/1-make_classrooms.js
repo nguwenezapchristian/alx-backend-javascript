ac3fb51a080e70b786e49054143fcfe9ee3b66e6
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-25 13:29:48 +0200

    camelcase

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,11 +2,11 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   let array = [];
-  const classroomObj = new ClassRoom(19);
-  array.push(classroomObj);
-  const classroomObj2 = new ClassRoom(20);
-  array.push(classroomObj2);
-  const classroomObj3 = new ClassRoom(34);
-  array.push(classroomObj3);
+  const classRoomObj = new ClassRoom(19);
+  array.push(classRoomObj);
+  const classRoomObj2 = new ClassRoom(20);
+  array.push(classRoomObj2);
+  const classRoomObj3 = new ClassRoom(34);
+  array.push(classRoomObj3);
   return array;
 }
