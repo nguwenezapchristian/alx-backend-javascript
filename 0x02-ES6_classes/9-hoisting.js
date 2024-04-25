677d9e124756acb11f853b96570faa5f5585b55f
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-25 21:05:22 +0200

    used this instead of self to fix the code and dealt with hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -28,11 +28,11 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
