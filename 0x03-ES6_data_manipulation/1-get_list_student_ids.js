df07b76810641fa3ec1a1d701bb2d3ba54cbe1b3
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-30 10:06:34 +0200

    removed brackets in if statement

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,7 @@
 import getListStudents from "./0-get_list_students";
 
 export default function getListStudentIds(arr) {
-    if (!(Array.isArray(arr))) {
+    if (!Array.isArray(arr)) {
         return [];
     }
     return arr.map((value) => value.id);
