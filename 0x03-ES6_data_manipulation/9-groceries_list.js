713d40e9960be76d71fa42fb15f475790ab18917
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-30 21:52:27 +0200

    eturns a map of groceries with name and quantity

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function groceriesList() {
+    let groceryList = new Map();
+    groceryList.set('Apples', 10);
+    groceryList.set('Tomatoes', 10);
+    groceryList.set('Pasta', 1);
+    groceryList.set('Rice', 1);
+    groceryList.set('Banana', 5);
+    return groceriesList;
+}
